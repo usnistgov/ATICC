7f56d9153024897c3e076bdc7d0bfc5c61cda546
dyiop <akashworkmail@gmail.com>
2020-10-30 11:21:46 -0400

    added ping test code

--- Coordinator/scenarios/ping.go
+++ Coordinator/scenarios/ping.go
@@ -2,6 +2,9 @@ package scenarios
 
 import (
 	"log"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type pingRequest struct {
@@ -12,12 +15,32 @@ type pingRequest struct {
 func (p pingRequest) Run() (*ScenarioResponse, error) {
 	log.Printf("Pinging address %s (%d times)", p.Address, p.Count)
 
-	// todo
+	// ping -c p.Count p.Address > /dev/null && echo "1" || echo "0"
+	// if "1" then it connected, otherwise not.
 
-	return &ScenarioResponse{
-		Success: true,
-		Summary: "",
-	}, nil
+	command := exec.Command("sh", "-c",
+		"ping -c "+strconv.Itoa(p.Count)+" "+p.Address+" > /dev/null && echo \"1\" || echo \"0\"")
+
+	out, err := command.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var response ScenarioResponse
+
+	if strings.HasPrefix(string(out), "1") {
+		response = ScenarioResponse{
+			Success: true,
+			Summary: "The console output was: \n" + string(out),
+		}
+	} else {
+		response = ScenarioResponse{
+			Success: false,
+			Summary: "The console output was: \n" + string(out),
+		}
+	}
+
+	return &response, nil
 }
 
 func (p pingRequest) GetType() string {
