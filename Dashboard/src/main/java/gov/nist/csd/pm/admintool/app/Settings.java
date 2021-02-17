d6f2b1ddb8c13e7eeaa00a8a113d2b6f5828dcda
selenaxiao <55109903+selenaxiao@users.noreply.github.com>
2021-02-17 13:31:19 -0500

    Add icons that indicate coordinator status

--- Dashboard/src/main/java/gov/nist/csd/pm/admintool/app/Settings.java
+++ Dashboard/src/main/java/gov/nist/csd/pm/admintool/app/Settings.java
@@ -5,10 +5,13 @@ import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.details.DetailsVariant;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import gov.nist.csd.pm.admintool.actions.Action;
+import gov.nist.csd.pm.admintool.services.RestService;
 
 
 @Tag("settings")
@@ -32,18 +35,37 @@ public class Settings extends VerticalLayout {
         setPadding(true);
 
 
-        // Unit Tester
+        Icon checkIcon = new Icon(VaadinIcon.CHECK);
+        checkIcon.setColor("green");
+        Icon closeIcon = new Icon(VaadinIcon.CLOSE);
+        closeIcon.setColor("red");
+        Icon loadingIcon = new Icon(VaadinIcon.COG);
+
         TextField URLInput = new TextField();
+        HorizontalLayout textAndIcon = new HorizontalLayout(URLInput, loadingIcon);
+        textAndIcon.setVerticalComponentAlignment(Alignment.CENTER);
+
+        // Unit Tester
         URLInput.addValueChangeListener((event) -> {
             if (event.getValue() == null || event.getValue().equals(""))
                 Action.coordinatorURL = null;
-            else
+            else {
                 Action.coordinatorURL = event.getValue();
+                if (RestService.checkCoordinatorStatus(Action.coordinatorURL + "/status")) {
+                    textAndIcon.removeAll();
+                    textAndIcon.add(URLInput, checkIcon);
+                }
+                else {
+                    textAndIcon.removeAll();
+                    textAndIcon.add(URLInput, closeIcon);
+                }
+            }
         });
         URLInput.setWidth("100%");
 
+
         coordinatorURL = new Details("Coordinator URL", null);
-        coordinatorURL.setContent(URLInput);
+        coordinatorURL.setContent(textAndIcon);
         coordinatorURL.getElement().getStyle()
                 .set("background", "#DADADA"); //#A0FFA0
         coordinatorURL.addThemeVariants(DetailsVariant.FILLED);
@@ -53,6 +75,15 @@ public class Settings extends VerticalLayout {
                     URLInput.setValue("");
                 else {
                     URLInput.setValue(Action.coordinatorURL);
+                    if (RestService.checkCoordinatorStatus(Action.coordinatorURL + "/status")) {
+                        textAndIcon.removeAll();
+                        textAndIcon.add(URLInput, checkIcon);
+                    }
+                    else {
+                        textAndIcon.removeAll();
+                        textAndIcon.add(URLInput, closeIcon);
+                    }
+
                 }
             }
         });
