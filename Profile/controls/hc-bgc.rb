407508799e8b81c0b8f4151ce827dbac0fa06694
jedshakarji <jed3@umbc.edu>
2021-04-21 16:39:17 -0400

    added hostname containment profiles

---
+++ Profile/controls/hc-bgc.rb
@@ -0,0 +1,52 @@
+# copyright: 2021, The Hackers
+
+title "Hostname Containmnet Tests for a Bad Guy Client"
+
+control "HC-Ingress-BGC" do
+  impact 0.7
+  title "Hostname Containment Ingress - Bad Guy Client"
+
+  # Running fwknop command
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should_not exist }
+    its('stderr') { should_not eq '' }
+    its('exit_status') { should_not eq 0 }
+  end
+  #Test containment for Green host
+  describe host('sdp-attacker1.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host
+  describe host('sdp-gateway.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+end
+
+control "HC-Transiting-BGC" do
+  impact 0.7
+  title "Hostname Containment Transiting - Bad Guy Client"
+
+  # Running fwknop command
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should_not exist }
+    its('stderr') { should_not eq '' }
+    its('exit_status') { should_not eq 0 }
+  end
+  #Test Containment for Green host
+  describe host('sdp-attacker1.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host
+  describe host('sdp-gateway.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Resilient Controller
+  describe host('sdp-controller.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+end
---
+++ Profile/controls/hc-ggc.rb
@@ -0,0 +1,52 @@
+# copyright: 2021, The Hackers
+
+title "Hostname Containment Tests for a Good Guy Client"
+
+control "HC-Ingress-GGC" do
+  impact 0.7
+  title "Hostname Containment Ingress - Good Guy Client "
+
+  # Running fwknop command
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should exist }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+  #Test Containment for Green host
+  describe host('sdp-attacker1.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+  #Test Containment for Blue host
+  describe host('sdp-gateway.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+end
+
+control "HC-Transiting-GGC" do
+  impact 0.7
+  title "Hostname Containment Transiting - Good Guy Client"
+
+  # Running fwknop command
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should exist }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+  #Test Containment for Green host
+  describe host('sdp-attacker1.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+  #Test Containment for Blue host
+  describe host('sdp-gateway.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+  #Test Containment for Resilient Controller
+  describe host('sdp-controller.e3lab.solutions') do #Checks all ports and protocols if none are specified
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+end
