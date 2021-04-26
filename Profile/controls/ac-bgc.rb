ed652422e998db0680b7b982464e58d5897abf99
selenaxiao <55109903+selenaxiao@users.noreply.github.com>
2021-04-26 12:45:14 -0400

    Added access control profiles

--- Profile/controls/ac-bgc.rb
+++ Profile/controls/ac-bgc.rb
@@ -8,24 +8,42 @@ control "AC-Ingress-BGC" do
 
   # Running fwknop command
   describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
-    # TODO: this should either not exist or should not work
-
-    # it { should exist }
-    # its('stderr') { should eq '' }
-    # its('exit_status') { should eq 0 }
+    it { should_not exist }
+    its('stderr') { should_not eq '' }
+    its('exit_status') { should_not eq 0 }
   end
 
-  # Checking if telnet is closed on blue machiene
+  # icmp (Blue Machine)
+  describe port('0.0.0.0', 57621) do
+    it { should_not be_listening }
+    its('protocols') { should_not include 'tcp' }
+  end
+
+  #7 - F-Force App (Blue Machine)
+  describe port('sdp-gateway.e3lab.solutions', 8282) do
+    it { should_not be_listening }
+    its('protocols') { should_not include 'tcp' }
+  end
+
+  #8 - ssh (Blue Machine)
+  describe port('sdp-gateway.e3lab.solutions', 2200) do
+    it { should_not be_listening }
+    its('protocols') { should_not include 'tcp' }
+  end
+
+  #9 - VNC (Blue Machine)
+  describe port('sdp-gateway.e3lab.solutions', 5901) do
+    it { should_not be_listening }
+    its('protocols') { should_not include 'tcp' }
+  end
+
+  #10 - Telnet (Blue Machine)
   describe port('sdp-gateway.e3lab.solutions', 23) do
     it { should_not be_listening }
     its('protocols') { should_not include 'tcp' }
   end
 
-  # Checking if ssh is closed on blue machiene
-  describe port('sdp-gateway.e3lab.solutions', 22) do
-    it { should_not be_listening }
-    its('protocols') { should_not include 'tcp' }
-  end
+
 end
 
 control "AC-Egress-BGC" do
@@ -33,7 +51,23 @@ control "AC-Egress-BGC" do
   title "Access Control Egress - Bad Guy Client"
 
   # Running fwknop command
-  # test step 1
-  # test step 2
-  # test step 3
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should_not exist }
+    its('stderr') { should_not eq '' }
+    its('exit_status') { should_not eq 0 }
+  end
+  # Egress steps
+end
+
+control "AC-Transiting-BGC" do
+  impact 0.7
+  title "Access Control Transiting - Bad Guy Client"
+
+  # Running fwknop command
+  describe command('fwknop —wget-cmd /usr/bin/wget -R -n service_gate') do
+    it { should_not exist }
+    its('stderr') { should_not eq '' }
+    its('exit_status') { should_not eq 0 }
+  end
+  # Transiting steps
 end
