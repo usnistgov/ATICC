1b54e67dfe484392c8f239c17a7a3d07768f6c9c
JedShakarji <jed3@umbc.edu>
2021-09-28 14:11:55 -0400

    added authenticated/unauthenticated tags to controls and incorporated unauthenticated tests into HC profile

--- Profile/Client/controls/hc.rb
+++ Profile/Client/controls/hc.rb
@@ -11,16 +11,17 @@ control "HC-Ingress-AC" do
   tag "TIC Version":"3.0"
   tag "Hostname Containment"
   tag "Ingress"
+  tag "Authenticated"
 
   # Running fwknop command
   #describe command('fwknop') do
   #  it { should exist }
   #end3
 
-  describe command(input('full_fwknop_command')) do
-    #its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
-  end
+  #describe command(input('full_fwknop_command')) do
+  #  its('stderr') { should eq '' }
+  #  its('exit_status') { should eq 0 }
+  #end
 
   #Test Containment for Green host ICMP
   describe host('0.0.0.0', port: input('icmp_port'), protocol: 'tcp') do
@@ -94,7 +95,8 @@ control "HC-Transiting-AC" do
   tag "Capability":"Network"
   tag "TIC Version":"3.0"
   tag "Hostname Containment"
-  tag "Transitiing"
+  tag "Transiting"
+  tag "Authenticated"
 
   # Running fwknop command
   #describe command('fwknop') do
@@ -190,5 +192,178 @@ control "HC-Transiting-AC" do
   describe host('sdp-controller.e3lab.solutions', port: input('telnet_port'), protocol: 'tcp') do
     it { should be_reachable }
     it { should be_resolvable }
+  end
+end
+
+
+
+title "Hostname Containment Tests for an Unauthenticated Client"
+
+control "HC-Ingress-UC" do
+  impact 0.7
+  title "Hostname Containment Ingress - Unauthenticated Client"
+  desc "Justification","...."
+  desc "This control ...."
+  tag "Capability":"Network"
+  tag "TIC Version":"3.0"
+  tag "Hostname Containment"
+  tag "Ingress"
+  tag "Unauthenticated"
+
+  # Running fwknop command
+  #describe command('fwknop') do
+  #  it { should_not exist }
+  #end
+
+  #describe command(input('full_fwknop_command')) do
+  #  its('stderr') { should_not eq '' }
+  #  its('exit_status') { should_not eq 0 }
+  #end
+
+  #Test Containment for Green host ICMP
+  describe host('0.0.0.0', port: input('icmp_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host F-Force App
+  describe host(input('green_machine_address'), port: input('fforce_app_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host SSH
+  describe host(input('green_machine_address'), port: input('ssh_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host VNC
+  describe host(input('green_machine_address'), port: input('vnc_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host Telnet
+  describe host(input('green_machine_address'), port: input('telnet_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+
+  #Test Containment for Blue host ICMP
+  describe host('0.0.0.0', port: input('icmp_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host F-Force App
+  describe host(input('blue_machine_address'), port: input('fforce_app_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host SSH
+  describe host(input('blue_machine_address'), port: input('ssh_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host VNC
+  describe host(input('blue_machine_address'), port: input('vnc_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host Telnet
+  describe host(input('blue_machine_address'), port: input('telnet_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+end
+
+control "HC-Transiting-UC" do
+  impact 0.7
+  title "Hostname Containment Transiting - Unauthenticated Client"
+  desc "Justification","...."
+  desc "This control ...."
+  tag "Capability":"Network"
+  tag "TIC Version":"3.0"
+  tag "Hostname Containment"
+  tag "Transiting"
+  tag "Unauthenticated"
+
+  # Running fwknop command
+  #describe command('fwknop') do
+  #  it { should_not exist }
+  #end
+
+  #describe command(input('full_fwknop_command')) do
+  #  its('stderr') { should_not eq '' }
+  #  its('exit_status') { should_not eq 0 }
+  #end
+
+  #Test Containment for Green host ICMP
+  describe host('0.0.0.0', port: input('icmp_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host F-Force App
+  describe host(input('green_machine_address'), port: input('fforce_app_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host SSH
+  describe host(input('green_machine_address'), port: input('ssh_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host VNC
+  describe host(input('green_machine_address'), port: input('vnc_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Green host Telnet
+  describe host(input('green_machine_address'), port: input('telnet_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+
+  #Test Containment for Blue host ICMP
+  describe host('0.0.0.0', port: input('icmp_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host F-Force App
+  describe host(input('blue_machine_address'), port: input('fforce_app_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host SSH
+  describe host(input('blue_machine_address'), port: input('ssh_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host VNC
+  describe host(input('blue_machine_address'), port: input('vnc_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Blue host Telnet
+  describe host(input('blue_machine_address'), port: input('telnet_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+
+  #Test Containment for Resilient Controller host F-Force App
+  describe host('sdp-controller.e3lab.solutions', port: input('fforce_app_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Resilient Controller host SSH
+  describe host('sdp-controller.e3lab.solutions', port: input('ssh_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Resilient Controller host VNC
+  describe host('sdp-controller.e3lab.solutions', port: input('vnc_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
+  end
+  #Test Containment for Resilient Controller host Telnet
+  describe host('sdp-controller.e3lab.solutions', port: input('telnet_port'), protocol: 'tcp') do
+    it { should_not be_reachable }
+    it { should_not be_resolvable }
   end
 end
