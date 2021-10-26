a16516bb6a4b097a0d6a90000e17702b96e0662a
selenaxiao <55109903+selenaxiao@users.noreply.github.com>
2021-10-26 13:57:54 -0400

    Added controls to gateway profile to check iptables rules

--- Profile/GW/controls/ns.rb
+++ Profile/GW/controls/ns.rb
@@ -1,16 +1,32 @@
 title "Network Segmentation Tests for Gateway"
 
-control "NS-Ingress" do
+control "NS-Ingress Unauthenticated" do
   impact 0.7
-  title "Network Segmentation Ingress"
+  title "Network Segmentation Ingress - Unauthenticated Client"
 
   tag "Capability":"Network"
   tag "TIC Version":"3.0"
   tag "Network Segmentation"
   tag "Ingress"
+  tag "Unauthenticated"
 
   describe iptables(chain:'INPUT') do
-    it { should have_rule('-P INPUT DROP') }
+    it { should_not have_rule('-P INPUT ACCEPT') }
+  end
+end
+
+control "NS-Ingress Authenticated" do
+  impact 0.7
+  title "Network Segmentation Ingress - Authenticated Client"
+
+  tag "Capability":"Network"
+  tag "TIC Version":"3.0"
+  tag "Network Segmentation"
+  tag "Ingress"
+  tag "Authenticated"
+
+  describe iptables(chain:'INPUT') do
+    it { should have_rule('-P INPUT ACCEPT') }
   end
 end
 
